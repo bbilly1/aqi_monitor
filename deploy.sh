9b3c0b06c5f31d69561c352fa69aeed8263bc3e5
simon <simobilleter@gmail.com>
2021-02-16 17:57:25 +0700

    added postgres to docker for later integration

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 # upload project to vps2
 
+# get last dyn
+wget data.lpb-air.com --output-document=backend/flask/dyn/air.json
+
 rsync --progress -a docker-compose.yml vps2:docker/
+rsync --progress -a env vps2:docker/
 rsync --progress -a --delete-after \
     --exclude dyn --exclude config.sample --exclude __pychache__ \
     backend vps2:docker/
