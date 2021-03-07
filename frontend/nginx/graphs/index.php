5ea110e4a4e531545e3399f6144edc09b4ab4a5d
simon <simobilleter@gmail.com>
2021-03-07 11:43:01 +0700

    added pm 2.4 and pm 10 to /graphs

--- frontend/nginx/graphs/index.php
+++ frontend/nginx/graphs/index.php
@@ -18,6 +18,9 @@
         <div class="content">
             <h1>Graphs</h1>
         </div>
+        <div class="content">
+            <h2>Last three days</h2>
+        </div>
         <div class="graph3 content">
             <div class="graph_item">
                 <p>Three days ago</p>
@@ -38,11 +41,51 @@
                 </a>
             </div>
         </div>
+        <div class="content divider">
+            <hr class="col_border">
+        </div>
+        <div class="content">
+            <h2>Particle Matter sizes</h2>
+            <p><b>There is no healthy level of pollution.</b> Particle matter (PM) are defined in two different sizes: PM 2.5 which represents particle sizes smaller than 2.5 &#xB5;m or less than 1/20th of the diameter of a human hair and PM 10 which represents particle sizer smaller than 10 &#xB5;m or 1/5th of the diameter of a human hair.</p>
+            <p>The <a href="https://www.who.int/news-room/fact-sheets/detail/ambient-(outdoor)-air-quality-and-health" target="_blank">WHO</a> is providing more details on their website regarding particle matter and their health implications. On <a href="https://en.wikipedia.org/wiki/Particulates" target="blank">Wikipedia</a> there are some interesting links to studies for further reading.</p>
+        </div>
+        <div class="graph2 content">
+            <div class="graph_item">
+                <a href="/dyn/pm25.png" data-lightbox="pm-bar">
+                    <img src="/dyn/pm25.png" alt="pm 2.5 bar chart">
+                </a>
+            </div>
+            <div>
+                <h3>PM 2.5</h3>
+                <p>Particle matter sizes smaller than 2.5&#xB5;m are the most problematic as these particles will find their way through the lungs into the bloodstream.</p>
+                <p>The WHO Air quality guideline values set a 25 &#xB5;g/m&sup3; 24-hour average as an upper level threshold. In the 10 days overview you can see:</p>
+                <p>Green: Daily average exposure below 25 &#xB5;g/m&sup3;<br>
+                Red: Daily average exposure above 25 &#xB5;g/m&sup3;</p>
+            </div>
+        </div>
+        <div class="graph2 content">
+            <div class="graph_item">
+                <a href="/dyn/pm10.png" data-lightbox="pm-bar">
+                    <img src="/dyn/pm10.png" alt="pm 10 bar chart">
+                </a>
+            </div>
+            <div>
+                <h3>PM 10</h3>
+                <p>The threshold for the daily average PM 10 exposure is set to 50 &#xB5;g/m&sup3; by the WHO. Particles this size can penetrate and lodge deep inside the lungs but are too big to enter the blood stream. For this reason the threshold is higher.</p>
+                <p>In the 10 days overview you can see:</p>
+                <p>Green: Daily average exposure below 50 &#xB5;g/m&sup3;<br>
+                Red: Daily average exposure above 50 &#xB5;g/m&sup3;</p>
+            </div>
+        </div>
+        <div class="content divider">
+            <hr class="col_border">
+        </div>
     </div>
     <div class="block_wrap">
         <div class="tagline_content content">
             <h2>Some more graphs are coming soon here!</h2>
         </div>
     </div>
+    <?php include($_SERVER['DOCUMENT_ROOT'] . '/incl/footer.html'); ?>
 </body>
 </html>
