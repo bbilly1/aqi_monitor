3c7ce744f9a183ef00141e4f7bec6585bfe53fed
simon <simobilleter@gmail.com>
2021-03-11 18:10:19 +0700

    adding helper files to sync up to deploy and update automatically

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@
 
 rsync --progress -a docker-compose.yml vps2:docker/
 rsync --progress -a env vps2:docker/
+rsync --progress -a --delete-after helper_scripts vps2:docker/
 rsync --progress -a --delete-after \
     --exclude dyn --exclude config.sample --exclude __pychache__ \
     backend vps2:docker/
