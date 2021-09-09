aaae24b486f727b2544701bff8a32fd7d774d632
simon <simobilleter@gmail.com>
2021-09-09 10:57:26 +0700

    speedup deployment with slim python image and build step outside of compose

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 # upload project to vps2
 
-rsync --progress -a docker-compose.yml vps2:docker/
-rsync --progress -a env vps2:docker/
-rsync --progress -a --delete-after helper_scripts vps2:docker/
-rsync --progress -a --delete-after nginx vps2:docker/
-rsync --progress -a --delete-after --exclude config.json.sample --exclude __pycache__ --exclude static/dyn \
-    web vps2:docker/
+host="vps2"
+
+rsync --progress -a docker-compose.yml "$host":docker/
+rsync --progress -a env "$host":docker/
+rsync --progress -a --delete-after helper_scripts "$host":docker/
+rsync --progress -a --delete-after nginx "$host":docker/
+rsync --progress -a --delete-after \
+    --exclude config.json.sample \
+    --exclude __pycache__ \
+    --exclude static/dyn \
+    web "$host":docker/
+
+ssh "$host" 'docker build -t bbilly1/lpb-air:latest docker/web'
+ssh "$host" 'docker-compose -f docker/ up -d'
 
 ##
 exit 0
