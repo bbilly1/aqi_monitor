0dc77b3d5409b0fa4332d60f8bfd70b6d3875bc7
simon <simobilleter@gmail.com>
2023-03-25 12:49:12 +0700

    delete pg source files for dbsync

--- helper_scripts/db_sync.sh
+++ helper_scripts/db_sync.sh
@@ -12,17 +12,25 @@ echo "---------------------------------------"
 printf "\n  -> backup\n"
 ssh $remote_host 'docker exec postgres pg_dump -U aqi | gzip > backup.gz'
 printf "\n  -> download\n"
-rsync --progress -r --delete-after -e ssh $remote_host:backup.gz /tmp/backup.gz
+rsync --progress -r --remove-source-files -e ssh $remote_host:backup.gz /tmp/backup.gz
 
 # sync
 printf "\n  -> sync\n"
-rsync --progress -r --delete-after /tmp/backup.gz -e ssh $local_host:backup
+rsync --progress -r --remove-source-files /tmp/backup.gz -e ssh $local_host:backup
 ssh $local_host 'gzip -df backup/backup.gz'
 
 # replace
 printf "\n  -> replace\n"
-ssh $local_host "docker exec -i postgres psql -U aqi -c 'DROP TABLE IF EXISTS aqi;'"
-ssh $local_host "docker exec -i postgres psql -U aqi -c 'DROP TABLE IF EXISTS weather;'"
+
+ssh $local_host "docker exec -i postgres psql -U aqi -c \
+    'DROP SCHEMA public CASCADE;'"
+ssh $local_host "docker exec -i postgres psql -U aqi -c \
+    'CREATE SCHEMA public;'"
+
+ssh $local_host "docker exec -i postgres psql -U aqi -c \
+    'DROP TABLE IF EXISTS aqi;'"
+ssh $local_host "docker exec -i postgres psql -U aqi -c \
+    'DROP TABLE IF EXISTS weather;'"
 ssh $local_host 'docker exec -i postgres psql -U aqi -d aqi < backup/backup'
 ssh $local_host "trash backup/backup"
 printf "\n  -> done\n"
