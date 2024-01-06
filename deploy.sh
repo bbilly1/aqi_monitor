a1aeb97e41d425c564c4e4b09ca06ab4d468b061
Simon <simobilleter@gmail.com>
2024-01-06 10:36:54 +0700

    remove legacy db sync function

--- deploy.sh
+++ deploy.sh
@@ -39,27 +39,12 @@ function sync_test {
 
 }
 
-# sync prod db to testing
-function sync_db {
-
-    newest_backup=$(ssh $remote_host 'find backup -type f -name "pg_*.gz" | sort | tail -n 1')
-    rsync --progress -e ssh $remote_host:"$newest_backup" .
-    rsync --progress -e ssh "$(basename "$newest_backup")" $local_host:backup/backup.gz
-
-    ssh $local_host "gzip -d backup/backup.gz"
-    ssh $local_host "docker exec -i postgres psql -U aqi -d aqi < backup/backup"
-    ssh $local_host "docker compose -f docker/docker-compose.yml restart"
-
-}
-
 if [[ $1 == "test" ]]; then
     sync_test "$2"
 elif [[ $1 == "docker" ]]; then
     sync_docker
-elif [[ $1 == "db" ]]; then
-    sync_db
 else
-    echo "valid options are: test | docker | db"
+    echo "valid options are: test | docker"
 fi
 
 ##
